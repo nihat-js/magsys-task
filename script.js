26bd297e6fae841197e763996f643cd6316d45e8
Nihat <abdullazadenihat@gmail.com>
2023-07-25 00:39:12 +0400

    fixed bugs

--- script.js
+++ script.js
@@ -2,34 +2,39 @@
 
 import Api from "./Api.js";
 
-async function main() {
+function renderCards(count) {
   let arr = []
-  for (let i = 1; i <= 4; i++) {
-    arr.push(Api.getFacts() )
+  let mainDiv = document.querySelector('.factsMain')
+  for (let i = 1; i <= count; i++) {
+    
+    const factItemDiv = document.createElement('div');
+    factItemDiv.classList.add('factItem');
+    
+    const factDescriptionDiv = document.createElement('div');
+    factDescriptionDiv.classList.add('factDesription');
+    factDescriptionDiv.setAttribute('id',i);
+    
+    const img = document.createElement('img');
+    img.src = "./images/loading.svg"
+    
+    factItemDiv.append(img,factDescriptionDiv);
+    
+    
+    mainDiv.append(factItemDiv)
+    Api.getFacts().then(response => {
+      factDescriptionDiv.textContent = response.fact
+      img.style.display = "none"
+    })
+    
   }
 
-  Promise.allSettled(arr).then(results=>{
-    let mainDiv = document.querySelector(".factList")
-    results.forEach( (result ,index )=> {
-      const {fact , lenght} = result.value
-      console.log(result);
-      const factItemDiv = document.createElement('div');
-      factItemDiv.classList.add('factItem');
-    
-      const factDescriptionDiv = document.createElement('div');
-      factDescriptionDiv.classList.add('factDesription');
-      factDescriptionDiv.setAttribute('id',index );
-      
-      factDescriptionDiv.innerText = fact;
-      factItemDiv.appendChild(factDescriptionDiv);
-
-      mainDiv.append(factItemDiv)
-      
-    })
-
-
-  })
+  // Promise.allSettled(arr).then(results=>{
+  //   let mainDiv = document.querySelector(".factList")
+  //   results.forEach( (result ,index )=> {      
+  //   })
+  // })
+  
+  
 }
 
-
-main()
+renderCards(4)
